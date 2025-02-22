9bdd32771432383d3fd7cedf1ae0c3c06e893602
Kacaii <kacaii.privado@gmail.com>
2025-02-22 10:11:27 -0300

    css is working

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { serveStatic } from "hono/deno";
 const app = new Hono();
 
 app.use("/static/*", serveStatic({ root: "./" }));
+app.use("/static/css/*", serveStatic({ root: "./css" }));
 
 app.get("/", serveStatic({ path: "./static/index.html" }));
+
+app.post("/clicked", (c) => c.text("Hello Hono! You <b>clicked</b> me!"));
 
 Deno.serve({ port: 3000, hostname: "localhost" }, app.fetch);
