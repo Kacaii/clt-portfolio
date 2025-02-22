f8500400a9494cc1aaf1bd0c7989f8a5d4a58f34
Kacaii <kacaii.privado@gmail.com>
2025-02-22 16:37:54 -0300

    add navbar

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ const app = new Hono();
 
 app.use("/static/*", serveStatic({ root: "./" }));
 
+// GET Requests
 app.get("/", serveStatic({ path: "./static/index.html" }));
+
+// POST Requests
+app.post("/main", (c) => c.text("swapped!"));
 
 Deno.serve({ port: 3000, hostname: "localhost" }, app.fetch);
