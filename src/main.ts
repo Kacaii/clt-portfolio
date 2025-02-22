475e76d4ea4799ce5958e420e3375e2920200885
Kacaii <kacaii.privado@gmail.com>
2025-02-22 10:21:06 -0300

    no need to serve css

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { serveStatic } from "hono/deno";
 const app = new Hono();
 
 app.use("/static/*", serveStatic({ root: "./" }));
-app.use("/static/css/*", serveStatic({ root: "./css" }));
 
 app.get("/", serveStatic({ path: "./static/index.html" }));
-
-app.post("/clicked", (c) => c.text("Hello Hono! You <b>clicked</b> me!"));
 
 Deno.serve({ port: 3000, hostname: "localhost" }, app.fetch);
